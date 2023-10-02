eb603a54e91e291c119fbcc97c8b72cb82dc34bd
Galo Sandoval <galosan92@gmail.com>
2023-10-01 19:40:47 -0600

    adds resume link for dev

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -10,7 +10,7 @@ export const Resume = ({
     <main className="flex h-[29.7cm] w-[21cm] justify-center bg-white text-black">
       <div>
         <h1 className="text-3xl uppercase">{resume?.name}</h1>
-        <h1 className="text-lg uppercase">{resume?.profession}</h1>
+        <h1 className="text-lg uppercase">{resume?.profession}</h1>Asdfasdf
       </div>
     </main>
   )
