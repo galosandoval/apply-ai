dec2229e16c0321d0b48937e67aaa15c6aa670d1
Galo Sandoval <galosan92@gmail.com>
2024-03-12 17:13:26 -0600

    adds new input

--- src/components/text-input.tsx
+++ src/components/text-input.tsx
@@ -1,11 +1,20 @@
 import {
   type UseFormRegister,
   type FieldValues,
-  type Path
+  type Path,
+  type ControllerRenderProps,
+  type FieldPath
 } from "react-hook-form"
 import { MyErrorMessage } from "./my-error-message"
-import { Input } from "./ui/input"
+import { Input, InputProps } from "./ui/input"
 import { Label } from "./ui/label"
+import {
+  FormControl,
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "./ui/form"
 
 type NameInputProps<T extends FieldValues> = {
   label: string
@@ -35,5 +44,40 @@ export function TextInput<T extends FieldValues>({
 
       <MyErrorMessage errors={errors} name={name} />
     </div>
+  )
+}
+
+interface TextInputProps<
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues>
+> extends InputProps {
+  label: string
+  description?: string
+  required?: boolean
+  field: ControllerRenderProps<TFieldValues, TName>
+}
+
+export function NewTextInput<
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues>
+>({
+  description,
+  label,
+  required,
+  field,
+  ...props
+}: TextInputProps<TFieldValues, TName>) {
+  return (
+    <FormItem className="w-full">
+      <FormLabel>
+        {label}
+        {required && <span className="text-destructive">*</span>}
+      </FormLabel>
+      <FormControl>
+        <Input {...field} {...props} />
+      </FormControl>
+      <FormDescription>{description}</FormDescription>
+      <FormMessage />
+    </FormItem>
   )
 }
