f8538a5663da7ecefa3c954fad7f737cbe996a63
Galo Sandoval <galosan92@gmail.com>
2024-03-13 10:10:15 -0600

    reverts change

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,5 @@
 export { default } from "next-auth/middleware"
 
 export const config = {
-  matcher: [
-    "/dashboard",
-    "/resume",
-    "/onboarding/experience",
-    "/onboarding/education",
-    "/onboarding/skills",
-    "/onboarding/contact"
-  ]
+  matcher: ["/dashboard", "/resume", "/onboarding/:path*"]
 }
